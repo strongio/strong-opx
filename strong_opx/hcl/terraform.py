73f28e00f36e547c938131e55ed56076439d8a9f
Adnan Umer <u.adnan@outlook.com>
2025-09-09 21:13:11 +0500

    fix: Remove hardcoded tfbackend path and look for *.tfbackend

--- strong_opx/hcl/terraform.py
+++ strong_opx/hcl/terraform.py
@@ -19,28 +19,38 @@ class TerraformRunner(HCLRunner):
 def run_terraform(environment: "Environment", command: str, *additional_args: str):
     project = environment.project
 
-    terraform_dir = environment.path
-    backend_file = os.path.join(environment.path, f"{environment.name}.s3.tfbackend")
+    backend_files = list(
+        os.path.join(environment.path, file) for file in os.listdir(environment.path) if file.endswith(".tfbackend")
+    )
+
+    if len(backend_files) == 0:
+        raise ImproperlyConfiguredError(
+            "No backend configuration file found. Please create a .tfbackend file in the environment directory."
+        )
+    elif len(backend_files) > 1:
+        raise ImproperlyConfiguredError(
+            "Multiple backend configuration files found. Please ensure there is only one "
+            ".tfbackend file in the environment directory."
+        )
 
     environ = dict(os.environ)
+    terraform_dir = os.path.join(project.path, "terraform")
 
-    if os.path.exists(backend_file):
-        terraform_dir = os.path.join(project.path, "terraform")
-        # By default, Terraform creates the .terraform directory in the same directory
-        # as the other .tf files. Using TF_DATA_DIR lets us create the .terraform dir
-        # wherever we want.
-        # Source: https://www.terraform.io/cli/config/environment-variables#tf_data_dir
-        terraform_data_dir = os.path.join(environment.path, ".terraform")
-        environ["TF_DATA_DIR"] = terraform_data_dir
+    # By default, Terraform creates the .terraform directory in the same directory
+    # as the other .tf files. Using TF_DATA_DIR lets us create the .terraform dir
+    # wherever we want.
+    # Source: https://www.terraform.io/cli/config/environment-variables#tf_data_dir
+    terraform_data_dir = os.path.join(environment.path, ".terraform")
+    environ["TF_DATA_DIR"] = terraform_data_dir
 
-        if command == "init":
-            additional_args += (f"-backend-config={backend_file}",)
+    if command == "init":
+        additional_args += (f"-backend-config={backend_files[0]}",)
 
-        # check to see if .terraform has been created
-        elif not os.path.exists(terraform_data_dir):
-            raise ImproperlyConfiguredError(
-                "Please run `strong-opx terraform init` prior to running other Terraform commands."
-            )
+    # check to see if .terraform has been created
+    elif not os.path.exists(terraform_data_dir):
+        raise ImproperlyConfiguredError(
+            "Please run `strong-opx terraform init` prior to running other Terraform commands."
+        )
 
     runner = TerraformRunner(environment=environment, directory=terraform_dir)
     runner.run(command, env=environ, additional_args=additional_args)
--- tests/hcl/test_terraform.py
+++ tests/hcl/test_terraform.py
@@ -14,23 +14,21 @@ class RunTerraformTests(TestCase):
         self.project.config = mock.Mock(terraform_executable="terraform")
 
     @mock.patch("strong_opx.hcl.runner.shell")
-    @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars")
-    def test_pass_additional_args(self, extract_vars_mock: mock.Mock, shell_mock: mock.Mock):
-        extract_vars_mock.return_value = {}
-
+    @mock.patch("strong_opx.hcl.runner.os.path.exists", new=mock.MagicMock(return_value=True))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=[".tfbackend"]))
+    @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    def test_pass_additional_args(self, shell_mock: mock.Mock):
         run_terraform(self.environment, "apply", "-target", "hello")
-        shell_mock.assert_called_once_with(
-            ("terraform", "apply", "-target", "hello"),
-            env={},
-            cwd=self.environment.path,
-        )
+        shell_mock.assert_called_once()
+        self.assertTupleEqual(shell_mock.call_args[0][0], ("terraform", "apply", "-target", "hello"))
 
     @mock.patch("strong_opx.hcl.runner.shell")
     @mock.patch("strong_opx.hcl.terraform.os.path.exists", new=mock.MagicMock(return_value=True))
     @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=["s3.tfbackend"]))
     def test_backend_config_for_init(self, shell_mock: mock.Mock):
         run_terraform(self.environment, "init")
-        backend_config_path = os.path.join(self.environment.path, f"{self.environment.name}.s3.tfbackend")
+        backend_config_path = os.path.join(self.environment.path, "s3.tfbackend")
         shell_mock.assert_called_once_with(
             ("terraform", "init", f"-backend-config={backend_config_path}"),
             env={"TF_DATA_DIR": os.path.join(self.environment.path, ".terraform")},
@@ -40,6 +38,7 @@ class RunTerraformTests(TestCase):
     @mock.patch("strong_opx.hcl.runner.shell")
     @mock.patch("strong_opx.hcl.terraform.os.path.exists", new=mock.MagicMock(return_value=True))
     @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=[".tfbackend"]))
     def test_backend_config_for_non_init(self, shell_mock: mock.Mock):
         run_terraform(self.environment, "apply")
         shell_mock.assert_called_once_with(
@@ -49,12 +48,38 @@ class RunTerraformTests(TestCase):
         )
 
     @mock.patch("strong_opx.hcl.runner.shell", new=mock.MagicMock())
-    @mock.patch("strong_opx.hcl.terraform.os.path.exists", new=mock.MagicMock(side_effect=[True, False]))
+    @mock.patch("strong_opx.hcl.terraform.os.path.exists", new=mock.MagicMock(return_value=False))
     @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=[".tfbackend"]))
     def test_backend_config_for_non_init__uninitialized(self):
         with self.assertRaises(ImproperlyConfiguredError) as cm:
             run_terraform(self.environment, "apply")
 
         self.assertEqual(
             str(cm.exception), "Please run `strong-opx terraform init` prior to running other Terraform commands."
+        )
+
+    @mock.patch("strong_opx.hcl.runner.shell", new=mock.MagicMock())
+    @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=[]))
+    def test_no_backend_file(self):
+        with self.assertRaises(ImproperlyConfiguredError) as cm:
+            run_terraform(self.environment, "apply")
+
+        self.assertEqual(
+            str(cm.exception),
+            "No backend configuration file found. Please create a .tfbackend file in the environment directory.",
+        )
+
+    @mock.patch("strong_opx.hcl.runner.shell", new=mock.MagicMock())
+    @mock.patch("strong_opx.hcl.terraform.TerraformRunner.extract_vars", new=mock.MagicMock(return_value={}))
+    @mock.patch("strong_opx.hcl.runner.os.listdir", new=mock.MagicMock(return_value=["a.tfbackend", "b.tfbackend"]))
+    def test_multiple_backend_file(self):
+        with self.assertRaises(ImproperlyConfiguredError) as cm:
+            run_terraform(self.environment, "apply")
+
+        self.assertEqual(
+            str(cm.exception),
+            "Multiple backend configuration files found. Please ensure there is only one .tfbackend "
+            "file in the environment directory.",
         )
