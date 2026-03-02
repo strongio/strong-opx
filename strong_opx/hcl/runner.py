a1dca39dc7574b0013adaf42962c507cb8633f77
Chris Aavang <chris.aavang@onesixsolutions.com>
2026-03-02 10:34:55 -0600

    fix(terraform,packer,cli): build strings for lists and dicts differently
    Simply str()'ing complex types does not create strings in the format that Terraform requires. I also tested and fixed dicts. The fixes apply to Packer as well.

--- strong_opx/hcl/runner.py
+++ strong_opx/hcl/runner.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from strong_opx.exceptions import UndefinedVariableError
 from strong_opx.hcl.extractor import HCLVariableExtractor
@@ -17,6 +17,51 @@ class HCLRunner:
     def __init__(self, environment: "Environment", directory: str):
         self.environment = environment
         self.directory = directory
+
+    @staticmethod
+    def _get_var_value(value: Any) -> str:
+        """
+        Converts a variable value to a string that can be used as an environment variable that will be passed to a
+        Terraform or Packer command.
+
+        Per Terraform documentation:
+        https://developer.hashicorp.com/terraform/cli/commands/plan#input-variables-on-the-command-line
+
+        string, number, and bool values are expected to be passed as strings with no special punctuation. For all other
+        type constraints, including list, map, and set types and the special any keyword, you must write a valid
+        Terraform language expression representing the value, and write any necessary quoting or escape characters to
+        ensure it will pass through your shell literally to Terraform.
+
+        Packer documentation does not explicitly call this out, but, in testing, this code works for Packer as well.
+
+        NOTE: This method does NOT handle Windows command line escaping, so it should only be used in Unix environments.
+        strong-opx does not currently support Windows, but I wanted to explicitly call it out here.
+        """
+        if isinstance(value, list) or isinstance(value, tuple):
+            build_string = "["
+            for item in value:
+                if isinstance(item, str):
+                    build_string += f'"{item}",'
+                else:
+                    build_string += f"{HCLRunner._get_var_value(item)},"
+            if build_string.endswith(","):
+                build_string = build_string[:-1]  # remove trailing comma
+            build_string += "]"
+            return build_string
+        elif isinstance(value, dict):
+            build_string = "{"
+            for key, val in value.items():
+                if isinstance(val, str):
+                    build_string += f'"{key}": "{val}",'
+                else:
+                    build_string += f'"{key}": {HCLRunner._get_var_value(val)},'
+            if build_string.endswith(","):
+                build_string = build_string[:-1]  # remove trailing comma
+
+            build_string += "}"
+
+            return build_string
+        return str(value)
 
     def extract_vars(self) -> dict[str, str]:
         env_dict = {}
@@ -36,14 +81,14 @@ class HCLRunner:
                 missing_vars.append(var)
                 continue
 
-            env_dict[f"{self.env_var_prefix}_{var}"] = str(context[var])
+            env_dict[f"{self.env_var_prefix}_{var}"] = self._get_var_value(context[var])
 
         if missing_vars:
             raise UndefinedVariableError(*missing_vars)
 
         for var in extractor.optional_vars:
             if var in context:
-                env_dict[f"{self.env_var_prefix}_{var}"] = str(context[var])
+                env_dict[f"{self.env_var_prefix}_{var}"] = self._get_var_value(context[var])
 
         return env_dict
 
