906ee4e0b1d50ffc932c71877170958923e606bd
Chris Aavang <93933324+chris-aavang@users.noreply.github.com>
2026-03-09 10:48:27 -0500

    fix: build strings for complex types differently for Terraform/Packer commands (#5)
    fix(terraform,packer,cli): build strings for lists and dicts differently
    
    Simply str()'ing complex types does not create strings in the format that Terraform requires. I also tested and fixed dicts. The fixes apply to Packer as well.

--- tests/hcl/test_runner.py
+++ tests/hcl/test_runner.py
@@ -55,15 +55,38 @@ class HCLRunnerTest(TestCase):
     @mock.patch("strong_opx.hcl.runner.HCLVariableExtractor")
     def test_required_and_optional_vars_with_none_missing(self, extractor_mock: mock.Mock, listdir_mock: mock.Mock):
         listdir_mock.return_value = []
-        extractor_mock.return_value.required_vars = ["VAR_1", "VAR_2"]
-        extractor_mock.return_value.optional_vars = ["VAR_3"]
+        extractor_mock.return_value.required_vars = ["VAR_1", "VAR_2", "ARRAY_OF_INTS", "MIXED_ARRAY", "TUPLE"]
+        extractor_mock.return_value.optional_vars = ["ARRAY_OF_STRINGS", "ARRAY_OF_ARRAYS_OF_STRINGS", "NESTED_MIX"]
 
         runner = TestHCLRunner()
-        runner.environment.context = Context({"VAR_1": "VAL_1", "VAR_2": "VAL_2", "VAR_3": "VAL_3"})
+        runner.environment.context = Context(
+            {
+                "VAR_1": "VAL_1",
+                "VAR_2": 2,
+                "ARRAY_OF_STRINGS": ["VAL_3_1", "VAL_3_2"],
+                "ARRAY_OF_INTS": [1, 2, 3],
+                "ARRAY_OF_ARRAYS_OF_STRINGS": [["VAL_4_1_1", "VAL_4_1_2"], ["VAL_4_2_1", "VAL_4_2_2"]],
+                "MIXED_ARRAY": ["VAL_5_1", 2, ["VAL_5_3_1", "VAL_5_3_2"]],
+                "NESTED_MIX": {"KEY1": "VAL_6_1", "KEY2": ["VAL_6_2_1", "VAL_6_2_2"]},
+                "TUPLE": ("TUPLE_1", "TUPLE_2"),
+            }
+        )
 
         actual_result = runner.extract_vars()
 
-        self.assertDictEqual({"T_VAR_1": "VAL_1", "T_VAR_2": "VAL_2", "T_VAR_3": "VAL_3"}, actual_result)
+        self.assertDictEqual(
+            {
+                "T_VAR_1": "VAL_1",
+                "T_VAR_2": "2",
+                "T_ARRAY_OF_STRINGS": '["VAL_3_1","VAL_3_2"]',
+                "T_ARRAY_OF_INTS": "[1,2,3]",
+                "T_ARRAY_OF_ARRAYS_OF_STRINGS": '[["VAL_4_1_1","VAL_4_1_2"],["VAL_4_2_1","VAL_4_2_2"]]',
+                "T_MIXED_ARRAY": '["VAL_5_1",2,["VAL_5_3_1","VAL_5_3_2"]]',
+                "T_NESTED_MIX": '{"KEY1": "VAL_6_1","KEY2": ["VAL_6_2_1","VAL_6_2_2"]}',
+                "T_TUPLE": '["TUPLE_1","TUPLE_2"]',
+            },
+            actual_result,
+        )
 
     @mock.patch("strong_opx.hcl.runner.os.listdir")
     @mock.patch("strong_opx.hcl.runner.HCLVariableExtractor")
